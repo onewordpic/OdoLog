fc1f092d78771deeded11721c09fddcbe30d117a
Safwan <safzmohd@outlook.com>
2026-09-05 11:42:21 +0530

    Initial Commit

---
+++ OdoLog/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OdoLog
+//
+//  Created by Safwan on 05/09/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OdoLog/OdoLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  OdoLogApp.swift
+//  OdoLog
+//
+//  Created by Safwan on 05/09/26.
+//
+
+import SwiftUI
+
+@main
+struct OdoLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
